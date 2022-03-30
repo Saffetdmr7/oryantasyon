f3d396fc1d40dd4898e81a3035c26265fe78cad4
Saffet Demir <95528329+Saffetdmr7@users.noreply.github.com>
2022-03-30 13:47:36 +0300

    Add files via upload

---
+++ World.java
@@ -0,0 +1,19 @@
+package world;
+
+public class World {
+
+    public static void main(String[] args) {
+        TV tv = new TV();
+        Ev ev = new Ev();
+        Ocak ocak = new Ocak();
+        Uydu uydu = new Uydu();
+        Kettle kettle = new Kettle();
+        Araba araba = new Araba();
+        Motor motor = new Motor();
+        AkıllıSaat saat = new AkıllıSaat();
+        KablosuzKulaklık kulaklık = new KablosuzKulaklık();
+        araba.ac();
+
+    }
+
+}
